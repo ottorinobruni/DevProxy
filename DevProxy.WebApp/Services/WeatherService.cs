5d49650c67c7bf2f80d283811bd046c739b9e0a8
Ottorino Bruni <ottorino.bruni@gmail.com>
2024-09-20 16:47:25 +0200

    Mock Server

--- DevProxy.WebApp/Services/WeatherService.cs
+++ DevProxy.WebApp/Services/WeatherService.cs
@@ -11,7 +11,7 @@
 
         public async Task<List<WeatherForecast>> GetWeatherForecasts()
         {
-            var response = await _httpClient.GetAsync("http://localhost:5246/weatherforecast");
+            var response = await _httpClient.GetAsync("https://63c1fbdc-2ba0-4d91-bb2a-7c130c1d5836.mock.pstmn.io/weatherforecast");
             response.EnsureSuccessStatusCode();
             var forecasts = await response.Content.ReadFromJsonAsync<IEnumerable<WeatherForecast>>();
             return forecasts?.ToList()!;
